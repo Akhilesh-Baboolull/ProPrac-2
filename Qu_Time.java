199a49b4eaf5633a456394624b3a15d12008967e
Akhilesh07 <51824203+Akhilesh-Baboolull@users.noreply.github.com>
2019-07-20 15:07:17 +0400

    Added comments

--- Qu_Time.java
+++ Qu_Time.java
@@ -22,20 +22,22 @@ public class Qu_Time {
 		
 		do {
 		System.out.println("Conversion Seconds to Minutes");
-		System.out.print("Enter number of seconds to be converted: ");
+			
+		System.out.print("Enter number of seconds to be converted: "); 
+		seconds = myInput.nextDouble(); //Input number of seconds
 		
-		seconds = myInput.nextDouble();
-		
-		minutes = seconds / 60;
-		
-		System.out.println(seconds + " seconds converted into minutes is: " + minutes + " minutes.");
+		minutes = seconds / 60; //calculation of minutes
+			
+		System.out.println(seconds + " seconds converted into minutes is: " + minutes + " minutes."); //printing the result
+		//Displaying a menu if the user wants to re-try
 		System.out.println("Choose 1 to Re-Try");
 		System.out.println("Choose 2 to Exit the program");
+			
 		 do {
 			 System.out.print("Choose whether to re-try or exit the program: ");
 		choice = myInput.nextInt();
 		
-		if (choice != 1 && choice != 2) {
+		if (choice != 1 && choice != 2) { //validating the choice entered
 			System.out.println("Invalid choice! Please Re-Enter..");
 		}
 		
