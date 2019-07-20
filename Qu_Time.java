01c7ba0ff1cc209a0faca37221928cfd8513929d
Akhilesh07 <51824203+Akhilesh-Baboolull@users.noreply.github.com>
2019-07-20 15:03:32 +0400

    First Commit

---
+++ Qu_Time.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+/**
+ * @author Akhilesh
+ * Version 1.0
+ *
+ */
+
+
+
+public class Qu_Time {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner myInput = new Scanner(System.in);
+		
+		
+		double seconds;
+		double minutes;
+		int choice;
+		
+		do {
+		System.out.println("Conversion Seconds to Minutes");
+		System.out.print("Enter number of seconds to be converted: ");
+		
+		seconds = myInput.nextDouble();
+		
+		minutes = seconds / 60;
+		
+		System.out.println(seconds + " seconds converted into minutes is: " + minutes + " minutes.");
+		System.out.println("Choose 1 to Re-Try");
+		System.out.println("Choose 2 to Exit the program");
+		 do {
+			 System.out.print("Choose whether to re-try or exit the program: ");
+		choice = myInput.nextInt();
+		
+		if (choice != 1 && choice != 2) {
+			System.out.println("Invalid choice! Please Re-Enter..");
+		}
+		
+		 }while(choice != 1 && choice != 2);
+		 
+		 if (choice == 2){
+			 System.out.print("You are exiting the program!"); 
+		 }
+		 
+		}while(choice == 1);
+		
+		
+		
+		
+		myInput.close();
+		
+		
+		
+	}
+
+}
